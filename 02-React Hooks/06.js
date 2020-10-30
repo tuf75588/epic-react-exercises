c3e0c4db7ee82a8fdadeabb29b9f05824c0fb011
Andrew Davis <atd285@gmail.com>
2020-10-29 22:39:40 -0400

    add an error boundary component

--- 02-React Hooks/06.js
+++ 02-React Hooks/06.js
@@ -16,7 +16,20 @@ import {
 function PokemonInfo({ pokemonName }) {
   const [pokemon, setPokemon] = React.useState(null);
   const [status, setStatus] = React.useState('idle');
-
+  // 🐨 Have state for the pokemon (null)
+  // 🐨 use React.useEffect where the callback should be called whenever the
+  // pokemon name changes.
+  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
+  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
+  // 🐨 before calling `fetchPokemon`, make sure to update the loading state
+  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
+  //   fetchPokemon('Pikachu').then(
+  //     pokemonData => { /* update all the state here */},
+  //   )
+  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
+  //   1. no pokemonName: 'Submit a pokemon'
+  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
+  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
   React.useEffect(() => {
     setStatus('fetching');
     if (!pokemonName) return;
@@ -38,6 +51,29 @@ function PokemonInfo({ pokemonName }) {
   );
 }
 
+// error boundary component
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+  componentDidCatch(error, errorInfo) {
+    console.error({ error, errorInfo });
+  }
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong</h1>;
+    }
+    return this.props.children;
+  }
+}
+
 function App() {
   const [pokemonName, setPokemonName] = React.useState('');
 
@@ -46,13 +82,15 @@ function App() {
   }
 
   return (
-    <div className="pokemon-info-app">
-      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
-      <hr />
-      <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+    <ErrorBoundary>
+      <div className="pokemon-info-app">
+        <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+        <hr />
+        <div className="pokemon-info">
+          <PokemonInfo pokemonName={pokemonName} />
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
